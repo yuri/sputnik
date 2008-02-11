5ed349c33efaccdfe1663dd33e25ab450f9d22ca
takhteyev <takhteyev@b677e369-542f-0410-a5c7-ef776a037072>
2008-02-11 03:53:58 +0000

    Fixing the worldclock demo for the new version
    git-svn-id: http://sputnik-wiki.googlecode.com/svn/trunk/rocks@192 b677e369-542f-0410-a5c7-ef776a037072

--- sputnik-worldclock/lua/worldclock/sputnik/actions/worldclock.lua
+++ sputnik-worldclock/lua/worldclock/sputnik/actions/worldclock.lua
@@ -4,18 +4,18 @@ require("cosmo")
 
 actions = {}
 
-actions.show_content = function(page, params, sputnik)
-   local data = sputnik:load_config_page(page.name)
+actions.show_content = function(node, params, sputnik)
+
    city_by_tz = {}
-   for i, city in ipairs(data.cities) do
+   for i, city in ipairs(node.content.cities) do
       city_by_tz[city[2]] = city
    end
 
-   return cosmo.fill(page.templates.WORLD_CLOCK, {
+   return cosmo.fill(node.templates.WORLD_CLOCK, {
       do_hour = function()
 	 for i=1,24 do
-	    local slot = i + data.start
-	    local t = os.date("*t", os.time() + 3600*(slot-data.local_tz))
+	    local slot = i + node.content.start
+	    local t = os.date("*t", os.time() + 3600*(slot-node.content.local_tz))
 	    local city = city_by_tz[slot]
 	    local city_name
 	    if city then city_name = city[1] end
@@ -33,11 +33,10 @@ actions.show_content = function(page, params, sputnik)
    })
 end
 
-actions.full_html = function(page, params, sputnik)
-   local data = sputnik:load_config_page(page.name)
-   page.inner_html = cosmo.fill (page.templates.TIMED_UPDATE, {
-				    url = page.urls:show_content(),
-				    timeout = data.timeout * 1000 })
-   return page.wrappers.default(page, params, sputnik)
+actions.full_html = function(node, params, sputnik)
+   node.inner_html = cosmo.fill (node.templates.TIMED_UPDATE, {
+				    url = node.urls:show_content(),
+				    timeout = node.content.timeout * 1000 })
+   return node.wrappers.default(node, params, sputnik)
 end
 

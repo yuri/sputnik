4dd23a521df9bddb450334c22e85b1954a473d24
James Whitehead <jnwhiteh@artificialtruth.local>
2008-03-20 19:52:30 +0000

    Revamped the authentication.simple module to be clearer on the API
    * The code was gutted and all references to it throughout the wiki were changed
    * The module now uses my standard for OO declarations, with proper privacy where appropriate
    * The module will not be backwards compatible with old password files due to the change in user timestamps.  This could be altered, but I don't see the reason for the complexity at that level.
    * AUTHENTICATION_MODULE_PARAMS takes the following options for simple
     - node - The name of the node to use for the password file, defaults to _passwords
     - salt - The secret code (salt) used for the passwords
     - recent - The amount of time (in seconds) that a user is considered "recent" within.  This defaults to 14 * 24 * 60 * 60 (Which is 14 days)
     - NO_AUTO_REGISTRATION - When set to true, user accounts will not be created automatically
    * I actually don't believe this last parameter should be in the authentication module, but should be instead handled by the wiki.  I'm not clear on how this all should be designed, yet.

--- sputnik/lua/sputnik/authentication/simple.lua
+++ sputnik/lua/sputnik/authentication/simple.lua
@@ -1,113 +1,182 @@
 module(..., package.seeall)
--------------------------------------------------------------------------------
--- Authentication -------------------------------------------------------------
--------------------------------------------------------------------------------
 
-PASSWORD_TEMPLATE = [=[USERS = {}
+--[[--------------------------------------------------------------
+--  sputnik.authentication.simple
+--
+--  This is the reference implementation for an authentication
+--  module.  It implements all of the core functionality that
+--  sputnik will expect from a drop-in-replacement
+--------------------------------------------------------------]]--
+
+local PASSWORD_TEMPLATE = [=[USERS = {}
 $do_users[[USERS["$user"]={hash="$hash", time="$time"}
 ]]]=]
 
-function make_authenticator (sputnik, params)
-   params = params or {}
+local Simple = {}
+local Simple_mt = {__metatable = {}, __index = Simple}
 
-   local password_node = sputnik:get_node(sputnik.config.PASS_PAGE_NAME)
-   local users = password_node.content
-   users = users.USERS or {}
-
-   local function get_token(username) 
-      return md5.sumhexa(username..sputnik.config.SECRET_CODE.."Sputn!k") 
-   end
-   
-   local function get_token_for_timestamp(timestamp)
-      return md5.sumhexa(timestamp..sputnik.config.SECRET_CODE)
-   end
-
-   local function user_exists(username) 
-      return users[username]
-   end
-
-   local function get_salted_hash(time, password)
-      return md5.sumhexa(time..sputnik.config.SECRET_CODE..password)
-   end
-  
-   local function now() 
-      local d = os.date("*t")
-      return string.format("%04d-%02d-%02dT%02d-%02d-%02d", d.year, d.month, d.day, d.hour, d.min, d.sec)
-   end
-
-   local function days_since_ce(date)
-      return 365*tonumber(date:sub(0,4))+31*tonumber(date:sub(6,7))+tonumber(date:sub(9,10))
-   end
-   
-   local function hours_since_ce(timestamp)
-      return days_since_ce(timestamp)*24+tonumber(timestamp:sub(12,13))
-   end   
-   
-   local function is_recent(username)
-      if username and users[username] and users[username].time then
-         return (days_since_ce(now()) -days_since_ce(users[username].time)) < 14
-      else
-         --sputnik.logger:debug(username.." not known, will return false")
-         return false
-      end
-   end
-    
-   local function add_user(username, password) 
-      sputnik.logger:debug("adding a new user: "..username..","..password)
-      local time = now()
-      users[username] = {
-         hash  = get_salted_hash(time, password), 
-         time = time
-      }
-   end
-
-   local function save(user)
-      sputnik.logger:debug("current users:")
-      for k,v in pairs(users) do
-         sputnik.logger:debug(k)
-      end
-      local new_values = {
-         content = cosmo.f(PASSWORD_TEMPLATE){ 
-                      do_users = function()
-                         for user, value in pairs(users) do
-                            cosmo.yield{
-                               user=user, 
-                               hash=value.hash,
-                               time=value.time}
-                         end
-                      end
-                   },
-      }
-      password_node = sputnik:update_node_with_params(password_node, new_values)
-      password_node:save(user, "Added new user: "..user, {minor="yes"})
-   end
-
-   local function check_password(user, password) 
-      if users[user] and users[user].hash == get_salted_hash(users[user].time, password) then
-         return user, get_token(user)
-      elseif user_exists(user) or (params.NO_AUTO_REGISTRATION and user~="Admin") then
-          return nil
-      else
-	      add_user(user, password)
-	      save(user)
-	      return user, get_token(user)
-      end
-   end
-
-   local function check_token(user, token)
-      if ((user or ""):len() > 0) and (get_token(user) == token) then
-	      return user
-      else
-	      return nil
-      end
-   end
-
-   return { 
-      check_password = check_password,
-      check_token    = check_token,
-      is_recent      = is_recent,
-      get_token_for_timestamp = get_token_for_timestamp,
-      now            = now,
-      hours_since_ce = hours_since_ce,
-   }
+-- Utility functions
+local function load_users(sputnik, name)
+   local node = sputnik:get_node(name)
+   return node.content.USERS
 end
+
+local function get_salted_hash(time, salt, password)
+   assert(password, debug.traceback():gsub("\n", "<br>"))
+   return md5.sumhexa(time .. salt .. password)
+end
+
+local function user_token(user, salt)
+   assert(salt, debug.traceback():gsub("\n", "<br>"))
+   return md5.sumhexa(user .. salt .. "Sputnik")
+end
+
+------------------------------------------------------------------
+-- Creates a new instance of the authentication module for use
+-- in sputnik
+--
+-- @param sputnik the sputnik instance to use for storage
+-- @param params a table of configuration paramaters.  The
+-- requirements of this table depend on the specific module
+-- implementation.
+function new(sputnik, params)
+   -- Set up default parameters
+   params = params or {}
+   params.node = params.node or "_passwords"
+   params.salt = params.salt or sputnik.config.SECRET_CODE
+   params.recent = params.recent or (14 * 24 * 60 * 60)
+
+   local obj = setmetatable({}, Simple_mt)
+   obj.sputnik = sputnik
+   obj.node = params.node
+   obj.salt = params.salt
+   obj.noauto = params.NO_AUTO_REGISTRATION
+   obj.recent = params.recent
+
+   return obj
+end
+
+------------------------------------------------------------------
+-- Returns whether or not a given username exists in the 
+-- authentication system, without any further information
+--
+-- @param username the username to query
+-- @return exists whether or not the username exists in the system
+
+function Simple:user_exists(username)
+   local users = load_users(self.sputnik, self.node)
+   return type(users[username]) == "table"
+end
+
+------------------------------------------------------------------
+-- Returns a token for the specified timestamp.  This is provided
+-- for use outside the authentication system
+--
+-- @param timestamp - the timestamp to use when generating token
+-- @return token a hashed token representing the given timestamp
+
+function Simple:timestamp_token(timestamp)
+   return md5.sumhexa(timestamp .. self.salt)
+end
+
+------------------------------------------------------------------
+-- Attempt to authenticate a given user with a given password
+--
+-- @param user the username to authenticate
+-- @param password the raw password to authenticate with
+-- @return user the name of the authenticated user
+-- @return token a hashed token for the user
+
+function Simple:authenticate(user, password)
+   local users = load_users(self.sputnik, self.node)
+   local entry = users[user]
+
+   if entry and entry.hash == get_salted_hash(entry.time, self.salt, password) then
+      return user, get_token(user)
+   elseif self:user_exists(user) or (self.noauto and user ~= "Admin") then
+      return nil
+   else
+      self:add_user(user, password)
+      return user, user_token(user, self.salt)
+   end
+end
+
+------------------------------------------------------------------
+-- Validate an existing authentication token.  This is used for 
+-- allowing authentication via cookies
+--
+-- @param user the username the token belong to
+-- @param token the actual token hash
+-- @return user the name of the authenticated user
+
+function Simple:validate_token(user, token)
+   assert(self.salt, debug.traceback():gsub("\n", "<br>"))
+   if self:user_exists(user) and user_token(user, self.salt) == token then
+      return user
+   else
+      return nil
+   end
+end
+
+------------------------------------------------------------------
+-- Returns whether or not a given user is a new user, defined
+-- by the "recent" configuration parameter.
+-- @param user the username to query
+-- @return isRecent a boolean value indicating if the user's 
+-- account was created in the specified time frame
+
+function Simple:user_is_recent(user)
+   local users = load_users(self.sputnik, self.node)
+   local entry = users[user]
+
+   if entry then
+      local now = os.time()
+      local min = now - self.recent
+
+      return (entry.time > min)
+   else
+      return false
+   end
+end
+
+------------------------------------------------------------------
+-- Adds a user/password pair to the password file
+--
+-- @param user the username to add
+-- @param password the raw password
+-- @return success a boolean value indicating if the add was 
+-- successful.
+-- @return err an error message if the add was not successful
+function Simple:add_user(user, password)
+   local now = os.time()
+   local users = load_users(self.sputnik, self.node)
+  
+   if self:user_exists(user) then
+      return false, "That user already exists"
+   end
+
+   users[user] = {
+      hash = get_salted_hash(now, self.salt, password),
+      time = now,
+   }
+
+   local password_node = self.sputnik:get_node(self.node)
+   local params = {
+      content = cosmo.f(PASSWORD_TEMPLATE){
+         do_users = function()
+            for user,entry in pairs(users) do
+               cosmo.yield{
+                  user = user,
+                  hash = entry.hash,
+                  time = entry.time,
+               }
+            end
+         end
+      }
+   }
+   password_node = self.sputnik:update_node_with_params(password_node, params)
+   password_node:save(user, "Added new user: " .. user, {minor="yes"})
+   return true
+end
+  
+-- vim:ts=3 ss=3 sw=3 expandtab

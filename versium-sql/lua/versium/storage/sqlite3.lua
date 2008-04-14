2a9f85f12b62ac56191e26ef2a6c581dbc4dda54
James Whitehead <jnwhiteh@artificialtruth.local>
2008-04-14 13:32:59 +0100

    Converted the SQLite3 versium driver to work with the saci refactoring.
    There may be some extra queries laying about, but I just need it working at this point.

--- versium-sql/lua/versium/storage/sqlite3.lua
+++ versium-sql/lua/versium/storage/sqlite3.lua
@@ -1,11 +1,23 @@
 -----------------------------------------------------------------------------
--- Provides an implementation of versium storage using the LuaSQL abstraction
--- layer.  This provides the lowest common denominator SQL definitions, and
--- it should work on all the provided drivers.
+-- Implements Versium API using using the LuaSQL driver for SQLite3.
+--
+-- (c) 2008  James Whitehead II (jnwhiteh@gmail.com)
+-- License: MIT/X, see http://sputnik.freewisdom.org/en/License
 -----------------------------------------------------------------------------
 
 module(..., package.seeall)
+local util = require("versium.util")
+local errors = require("versium.errors")
 require("luasql.sqlite3")
+
+-----------------------------------------------------------------------------
+-- A table that describes what this versium implementation can and cannot do.
+-----------------------------------------------------------------------------
+capabilities = {
+   can_save = true,
+   has_history = true,
+   is_persistent = true,
+}
 
 -----------------------------------------------------------------------------
 -- Prepares a SQL statement using placeholders.
@@ -59,72 +71,86 @@ CREATE TABLE IF NOT EXISTS %s (
   PRIMARY KEY (id)
 );]]
 
-Storage = {}
+-----------------------------------------------------------------------------
+-- A table representing the class.
+-----------------------------------------------------------------------------
+local SQLite3Versium = {}
+local SQLite3Versium_mt = {__metatable = {}, __index = SQLite3Versium}
 
 -----------------------------------------------------------------------------
--- Instantiates a new storage object.
+-- Instantiates a new SQLite3Versium object that represents a connection to
+-- a storage system.  This is the only function that this module exports.
 -- 
--- @param impl           the implementation module.
--- @param params         the parameters to pass to the implementation.
+-- @param params         a table of parameters
 -- @return               a new versium object.
 -----------------------------------------------------------------------------
-function Storage:new(params, versiumObject)
+function new(params)
   	-- Params table accepts the following:
 	-- prefix - A string that will be prepended to table names
 	-- connect - A list that is passed to the luasql connection function
-	-- Try to connect to the given database
 
+	-- Try to connect to the given database
   	local env = luasql.sqlite3()
 	local con = env:connect(unpack(params.connect))
 
 	if not con then
-		versium.storage_error("SQLite3 Versium: Could not connect to database")
+	   errors.could_not_initialize("Could not connect to SQLite3 database")
 	end
 
-	self.tables = {}
+   -- Create the new object
+	local obj = {con=con, versium=versium}
+	setmetatable(obj, SQLite3Versium_mt)
+
+	obj.tables = {}
 
 	-- Create the two data tables, if they don't already exist
 	local tables = {"node", "node_index"}
 
 	for idx,tbl in ipairs(tables) do
-		self.tables.node = string.format("%snode", params.prefix or "")
-		self.tables.node_index = string.format("%snode_index", params.prefix or "")
+		obj.tables.node = string.format("%snode", params.prefix or "")
+		obj.tables.node_index = string.format("%snode_index", params.prefix or "")
 
-      local cmd = prepare(schemas[tbl]:format(self.tables[tbl]))
+      local cmd = prepare(schemas[tbl]:format(obj.tables[tbl]))
       assert(con:execute(cmd))
 	end
 
-
 	-- Pre-build our queries
-	self.queries = {
-		GET_NODE_VERSION = string.format("SELECT id,data,version from %s WHERE id = %%s and version = %%s;", self.tables.node),
-		GET_NODE_LATEST = string.format("SELECT n.id,n.data,n.version FROM %s as n NATURAL JOIN %s WHERE n.id = %%s;", self.tables.node, self.tables.node_index),
-		GET_VERSION = string.format("SELECT max(version) as version FROM %s WHERE id = %%s;", self.tables.node),
-		GET_NODES = string.format("SELECT id FROM %s ORDER BY id;", self.tables.node_index),
- 		NODE_EXISTS = string.format("SELECT DISTINCT id FROM %s WHERE id = %%s;", self.tables.node),
-		INSERT_NODE = string.format("INSERT INTO %s (id,version,author,comment,timestamp,data) VALUES (%%s, %%s, %%s, %%s, %%s, %%s);", self.tables.node),
-		INSERT_INDEX = string.format("INSERT INTO %s (id, version) VALUES (%%s, %%s);", self.tables.node_index),
-		UPDATE_INDEX = string.format("UPDATE %s SET version=%%s WHERE id = %%s;", self.tables.node_index),
-		GET_METADATA = string.format("SELECT id,version,timestamp,author,comment FROM %s WHERE id = %%s ORDER BY timestamp;", self.tables.node),
-		GET_METADATA_LATEST = string.format("SELECT n.id,n.version,timestamp,author,comment FROM %s as n NATURAL JOIN %s WHERE id = %%s ORDER BY timestamp;", self.tables.node, self.tables.node_index),
+	obj.queries = {
+		GET_NODE_VERSION = string.format("SELECT id,data,version from %s WHERE id = %%s and version = %%s;", obj.tables.node),
+		GET_NODE_LATEST = string.format("SELECT n.id,n.data,n.version FROM %s as n NATURAL JOIN %s WHERE n.id = %%s;", obj.tables.node, obj.tables.node_index),
+		GET_VERSION = string.format("SELECT max(version) as version FROM %s WHERE id = %%s;", obj.tables.node),
+		GET_NODES = string.format("SELECT id FROM %s ORDER BY id;", obj.tables.node_index),
+		GET_NODES_PREFIX_LIMIT = string.format("SELECT id FROM %s WHERE n.id LIKE %%s ORDER BY id LIMIT %%s;", obj.tables.node_index),
+      GET_NODES_PREFIX = string.format("SELECT id FROM %s WHERE n.id LIKE %%s ORDER BY id;", obj.tables.node_index),
+      GET_NODES_LIMIT = string.format("SELECT id FROM %s ORDER BY id LIMIT %%s;", obj.tables.node_index),
+      NODE_EXISTS = string.format("SELECT DISTINCT id FROM %s WHERE id = %%s;", obj.tables.node),
+		INSERT_NODE = string.format("INSERT INTO %s (id,version,author,comment,timestamp,data) VALUES (%%s, %%s, %%s, %%s, %%s, %%s);", obj.tables.node),
+		INSERT_INDEX = string.format("INSERT INTO %s (id, version) VALUES (%%s, %%s);", obj.tables.node_index),
+		UPDATE_INDEX = string.format("UPDATE %s SET version=%%s WHERE id = %%s;", obj.tables.node_index),
+		GET_METADATA = string.format("SELECT id,version,timestamp,author,comment FROM %s WHERE id = %%s ORDER BY timestamp;", obj.tables.node),
+		GET_METADATA_LATEST = string.format("SELECT n.id,n.version,timestamp,author,comment FROM %s as n NATURAL JOIN %s WHERE id = %%s ORDER BY timestamp;", obj.tables.node, obj.tables.node_index),
 	}
-
-	-- Create the new object
-	local obj = {con=con, versium=versium}
-	setmetatable(obj, self)
-	self.__index = self
 
 	return obj 
 end
 
 -----------------------------------------------------------------------------
--- Returns a table representing the node with a given id.
+-- Returns the data stored in the node as a string and a table representing
+-- the node's metadata.  Returns nil if the node doesn't exist.  Throws an
+-- error if anything else goes wrong.
 --
--- @param id             the node id.
--- @param version        the desired version of the node (defaults to current).
--- @return               the node as table with its content in node.data.
+-- @param id             a node id.
+-- @param version        [optional] the desired version of the node (defaults
+--                       to current).
+-- @return               a byte-string representing the data stored in the
+--                       node or nil if the node could not be loaded or nil.
+-- @return               a table representing the metadata, including the
+--                       following fields (see get_node_history()) or nil.
+-- @see get_node_history
 -----------------------------------------------------------------------------
-function Storage:get_node(id, version)
+function SQLite3Versium:get_node(id, version)
+   assert(id)
+
 	-- Get the most recent version of the node
 	local cmd
 	if version then
@@ -135,30 +161,15 @@ function Storage:get_node(id, version)
 
 	-- Run the query to get the node
 	local cur = assert(self.con:execute(cmd))
-
-	-- If we still don't have a version
    local row = cur:fetch({}, "a")
+   cur:close()
 
    if not row then
-		versium.storage_error(version.errors.NODE_DOES_NOT_EXIST, tostring(id))
-	end
+      return nil
+   end
 
 	assert(row.data)
-
-	return row
-end
-
------------------------------------------------------------------------------
--- Returns a table representing a node that doesn't actually exist yet.
---
--- @return               the stub of the node as a table.
------------------------------------------------------------------------------
-function Storage:get_stub(id)
-   return {
-      version = "000000",
-      data = "",
-      id = id,
-   }
+	return row.data, self:get_node_info(id, version)
 end
 
 -----------------------------------------------------------------------------
@@ -167,7 +178,7 @@ end
 -- @param id             an id of an node.
 -- @return               true or false.
 -----------------------------------------------------------------------------
-function Storage:node_exists(id)
+function SQLite3Versium:node_exists(id)
 	assert(id)
 
 	local cmd = prepare(self.queries.NODE_EXISTS, id)
@@ -179,13 +190,51 @@ function Storage:node_exists(id)
 end
 
 -----------------------------------------------------------------------------
--- Returns a list of IDs of all nodes in the repository, in no particular order.
+-- Returns a table with the metadata for the latest version of the node. Same
+-- as get_node_history(id)[1] in case of this implementation.
 -- 
--- @return               a list of IDs.
+-- @param id             an id of an node.
+-- @return               the metadata for the latest version or nil.
 -----------------------------------------------------------------------------
-function Storage:get_node_ids()
+function SQLite3Versium:get_node_info(id)
+	assert(id)
+
+	-- Fetch the latest version number
+	local cmd = prepare(self.queries.GET_METADATA_LATEST, id)
+	local cur = assert(self.con:execute(cmd))
+	local row = cur:fetch({}, "a")
+	cur:close()
+
+	return row
+end
+
+-----------------------------------------------------------------------------
+-- Returns a list of existing node ids, up to a certain limit.  (If no limit
+-- is specified, all ids are returned.)  The ids can be optionally filtered
+-- by prefix.  (In this case, the limit applies to the number of ids that
+-- are being _returned_.)  The ids can be returned in any order.
+--
+-- @param prefix         [optional] a prefix to filter the ids (defaults to
+--                       "").
+-- @param limit          [optional] the maximum number of ids to return.
+-- @return               a list of node ids.
+-- @return               true if there are more ids left.
+-----------------------------------------------------------------------------
+function SQLite3Versium:get_node_ids(prefix, limit)
 	local nodes = {}
-	local cmd = prepare(self.queries.GET_NODES)
+   local cmd
+   if prefix and limit then
+      prefix = prefix .. "%"
+      cmd = prepare(self.queries.GET_NODES_PREFIX_LIMIT, prefix, limit)
+   elseif prefix then
+      prefix = prefix .. "%"
+      cmd = prepare(self.queries.GET_NODES_PREFIX, prefix)
+   elseif limit then
+      cmd = prepare(self.queries.GET_NODES_LIMIT, limit)
+   else
+      cmd = prepare(self.queries.GET_NODES)
+   end
+
 	local cur = self.con:execute(cmd)
 	local row = cur:fetch({}, "a")
 
@@ -208,7 +257,7 @@ end
 -- @param extra          any extra metadata (optional).
 -- @return               the version id of the new node.
 -----------------------------------------------------------------------------
-function Storage:save_version(id, data, author, comment, extra, timestamp)
+function SQLite3Versium:save_version(id, data, author, comment, extra, timestamp)
 	assert (id)
 	assert(data)
 	assert(author)
@@ -219,19 +268,11 @@ function Storage:save_version(id, data, author, comment, extra, timestamp)
 		timestamp = string.format("%02d-%02d-%02d %02d:%02d:%02d", t.year, t.month, t.day, t.hour, t.min, t.sec)
 	end
 
-	-- Escape all backslashes in the data, since it will be going through a gsub filter
-	data = data:gsub("\\", "\\\\")
-
 	-- Determine what the new version number will be
 	local cmd = prepare(self.queries.GET_VERSION, id)
 	local cur = self.con:execute(cmd)
 	local row = cur:fetch({}, "a")
 	cur:close()
-
-	local out = ""
-	for k,v in pairs(row) do
-		out = out .. "key: " .. tostring(k) .. " Value: " .. tostring(v)
-	end
 
 	local new
 	local version = row and tonumber(row.version)
@@ -272,7 +313,7 @@ end
 -- @return history  a list of tables representing the versions (the list will
 -- be empty if the node doesn't exist)
 -----------------------------------------------------------------------------
-function Storage:get_node_history(id, prefix)
+function SQLite3Versium:get_node_history(id, prefix)
 	assert(id)
 
 	local history = {}
@@ -290,37 +331,6 @@ function Storage:get_node_history(id, prefix)
 	cur:close()
 
 	return history
-end
-
------------------------------------------------------------------------------
--- Returns a table with the metadata for the latest version of the node. Same
--- as get_node_history(id)[1] in case of this implementation.
--- 
--- @param id             an id of an node.
--- @return               the metadata for the latest version or nil.
------------------------------------------------------------------------------
-function Storage:get_node_info(id)
-	assert(id)
-
-	-- Fetch the latest version number
-	local cmd = prepare(self.queries.GET_METADATA_LATEST, id)
-	local cur = assert(self.con:execute(cmd))
-	local row = cur:fetch({}, "a")
-	cur:close()
-
-	return row
-end
-
------------------------------------------------------------------------------
--- Creates a new storage object.
--- 
--- @param params         the parameters to pass to the implementation.
--- @param versium        a generic versium instance.
--- @return               the new versium storage object.
------------------------------------------------------------------------------
-
-function open(params, versium)
-   return Storage:new(params, versium)
 end
 
 -- vim:ts=3 ss=3 sw=3 expandtab

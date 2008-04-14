4d03779ba7ceca6b10f52d8e4b350646358af4ba
James Whitehead <jnwhiteh@artificialtruth.local>
2008-04-14 21:59:27 +0100

    Altered the basic CSS logic to allow for relative URLS
    This is useful for background-images and other image-based styles.  You can call $make_url in the .css file with two arguments (node and action).  A url is generated to replace the call in the template.  For example:
    
    .markItUpButton1 a {
    	        background-image:url($make_url{node = "markitup/images/markdown/h1", action="png"});
    }
    
    will be transformed to the appropriate URL

--- sputnik/lua/sputnik/actions/css.lua
+++ sputnik/lua/sputnik/actions/css.lua
@@ -3,7 +3,13 @@ module(..., package.seeall)
 actions = {}
 
 actions.css = function(page, params, sputnik)
-   return page.content, "text/css"
+   -- Run the css content through cosmo in order to allow for relative URLS
+   local content = cosmo.f(page.content){
+      make_url = function(arg)
+         return sputnik:make_url(arg.node, arg.action)
+      end,
+   }
+   return content, "text/css"
 end
 
 actions.fancy_css = function(page, params, sputnik)
@@ -25,3 +31,4 @@ actions.fancy_css = function(page, params, sputnik)
 
 end
 
+-- vim:ts=3 ss=3 sw=3 expandtab

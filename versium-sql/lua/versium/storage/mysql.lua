7b92f02b27752249079667b1326bf0dd1c078111
James Whitehead II <jnwhiteh@gmail.com>
2008-04-15 08:46:26 -0500

    Updated mysql versium driver to work again.  Because data is stored in blobs, we must escape it.

--- versium-sql/lua/versium/storage/mysql.lua
+++ versium-sql/lua/versium/storage/mysql.lua
@@ -1,10 +1,23 @@
 -----------------------------------------------------------------------------
--- Provides an implementation of versium storage using the LuaSQL abstraction
--- layer, specifically written for the provided MySQL driver.
+-- Implements Versium API using using the LuaSQL driver for MySQL.
+--
+-- (c) 2008  James Whitehead II (jnwhiteh@gmail.com)
+-- License: MIT/X, see http://sputnik.freewisdom.org/en/License
 -----------------------------------------------------------------------------
 
 module(..., package.seeall)
+local util = require("versium.util")
+local errors = require("versium.errors")
 require("luasql.mysql")
+
+-----------------------------------------------------------------------------
+-- A table that describes what this versium implementation can and cannot do.
+-----------------------------------------------------------------------------
+capabilities = {
+   can_save = true,
+   has_history = true,
+   is_persistent = true,
+}
 
 -----------------------------------------------------------------------------
 -- Prepares a SQL statement using placeholders.
@@ -22,7 +35,6 @@ local function prepare(statement, ...)
         for index = 1, count do
             local value = select(index, ...)
             local type = type(value)
-
             
             if type == 'string' then
                 value = '\'' .. value:gsub('\'', '\'\'') .. '\''
@@ -43,91 +55,102 @@ end
 
 local schemas = {}
 schemas.node = [[
-CREATE TABLE %s ( 
-  id VARCHAR(255) NOT NULL,
-  version INT UNSIGNED NOT NULL,
-  author VARCHAR(255) NOT NULL,
-  comment TEXT,
-  timestamp CHAR(19) NOT NULL,
-  data MEDIUMBLOB NOT NULL,
-  PRIMARY KEY(id, version)
+CREATE TABLE IF NOT EXISTS %s ( 
+   id VARCHAR(255) NOT NULL,
+   version INT UNSIGNED NOT NULL AUTO_INCREMENT,
+   author VARCHAR(255) NOT NULL,
+   comment TEXT,
+   timestamp CHAR(19) NOT NULL,
+   data MEDIUMBLOB NOT NULL,
+   PRIMARY KEY(id, version)
 );]]
 schemas.node_index = [[
-CREATE TABLE %s (
-  id VARCHAR(255) NOT NULL,
-  version INT UNSIGNED NOT NULL,
-  PRIMARY KEY (id)
+CREATE TABLE IF NOT EXISTS %s (
+   id VARCHAR(255) NOT NULL,
+   version INT UNSIGNED NOT NULL,
+   PRIMARY KEY (id)
 );]]
 
-Storage = {}
+-----------------------------------------------------------------------------
+-- A table representing the class.
+-----------------------------------------------------------------------------
+local MySQLVersium = {}
+local MySQLVersium_mt = {__metatable = {}, __index = MySQLVersium}
 
 -----------------------------------------------------------------------------
--- Instantiates a new storage object.
+-- Instantiates a new MySQLVersium object that represents a connection to
+-- a storage system.  This is the only function that this module exports.
 -- 
--- @param impl           the implementation module.
--- @param params         the parameters to pass to the implementation.
+-- @param params         a table of parameters
 -- @return               a new versium object.
 -----------------------------------------------------------------------------
-function Storage:new(params, versiumObject)
-	-- Params table accepts the following:
+function new(params)
+  	-- Params table accepts the following:
 	-- prefix - A string that will be prepended to table names
 	-- connect - A list that is passed to the luasql connection function
 
-	local env = luasql.mysql()
+	-- Try to connect to the given database
+  	local env = luasql.mysql()
 	local con = env:connect(unpack(params.connect))
 
 	if not con then
-		versium.storage_error("MySQL Versium: Could not connect to database")
+	   errors.could_not_initialize("Could not connect to MySQL database")
 	end
 
-	self.tables = {}
+   -- Create the new object
+	local obj = {con=con, versium=versium}
+	setmetatable(obj, MySQLVersium_mt)
 
-	-- Check to verify that the given tables exist
+	obj.tables = {}
+
+	-- Create the two data tables, if they don't already exist
 	local tables = {"node", "node_index"}
+
 	for idx,tbl in ipairs(tables) do
-		self.tables.node = string.format("%snode", params.prefix or "")
-		self.tables.node_index = string.format("%snode_index", params.prefix or "")
+		obj.tables.node = string.format("%snode", params.prefix or "")
+		obj.tables.node_index = string.format("%snode_index", params.prefix or "")
 
-		local cmd = prepare("SHOW TABLES LIKE %s;", self.tables[tbl])
-		local cur = assert(con:execute(cmd))
-		if cur:numrows() < 1 then
-			-- Create table using schema
-			local cmd = prepare(schemas[tbl]:format(self.tables[tbl]))
-			assert(con:execute(cmd))
-		end
+      local cmd = prepare(schemas[tbl]:format(obj.tables[tbl]))
+      assert(con:execute(cmd))
 	end
-
 
 	-- Pre-build our queries
-	self.queries = {
-		GET_NODE_VERSION = string.format("SELECT id,data,version from %s WHERE id = %%s and version = %%s;", self.tables.node),
-		GET_NODE_LATEST = string.format("SELECT n.id,n.data,n.version FROM %s as n NATURAL JOIN %s WHERE n.id = %%s;", self.tables.node, self.tables.node_index),
-		GET_VERSION = string.format("SELECT max(version) as version FROM %s WHERE id = %%s;", self.tables.node),
-		GET_NODES = string.format("SELECT id FROM %s ORDER BY id;", self.tables.node_index),
- 		NODE_EXISTS = string.format("SELECT DISTINCT id FROM %s WHERE id = %%s;", self.tables.node),
-		INSERT_NODE = string.format("INSERT INTO %s (id,version,author,comment,timestamp,data) VALUES (%%s, %%s, %%s, %%s, %%s, %%s);", self.tables.node),
-		INSERT_INDEX = string.format("INSERT INTO %s (id, version) VALUES (%%s, %%s);", self.tables.node_index),
-		UPDATE_INDEX = string.format("UPDATE %s SET version=%%s WHERE id = %%s;", self.tables.node_index),
-		GET_METADATA = string.format("SELECT id,version,timestamp,author,comment FROM %s WHERE id = %%s ORDER BY timestamp;", self.tables.node),
-		GET_METADATA_LATEST = string.format("SELECT n.id,n.version,timestamp,author,comment FROM %s as n NATURAL JOIN %s WHERE id = %%s ORDER BY timestamp;", self.tables.node, self.tables.node_index),
+	obj.queries = {
+		GET_NODE_VERSION = string.format("SELECT id,data,version from %s WHERE id = %%s and version = %%s;", obj.tables.node),
+		GET_NODE_LATEST = string.format("SELECT n.id,n.data,n.version FROM %s as n NATURAL JOIN %s WHERE n.id = %%s;", obj.tables.node, obj.tables.node_index),
+		GET_VERSION = string.format("SELECT max(version) as version FROM %s WHERE id = %%s;", obj.tables.node),
+		GET_NODES = string.format("SELECT id FROM %s ORDER BY id;", obj.tables.node_index),
+		GET_NODES_PREFIX_LIMIT = string.format("SELECT id FROM %s WHERE id LIKE %%s ORDER BY id LIMIT %%s;", obj.tables.node_index),
+      GET_NODES_PREFIX = string.format("SELECT id FROM %s WHERE id LIKE %%s ORDER BY id;", obj.tables.node_index),
+      GET_NODES_LIMIT = string.format("SELECT id FROM %s ORDER BY id LIMIT %%s;", obj.tables.node_index),
+      NODE_EXISTS = string.format("SELECT DISTINCT id FROM %s WHERE id = %%s;", obj.tables.node),
+		INSERT_NODE = string.format("INSERT INTO %s (id,version,author,comment,timestamp,data) VALUES (%%s, %%s, %%s, %%s, %%s, %%s);", obj.tables.node),
+		INSERT_INDEX = string.format("INSERT INTO %s (id, version) VALUES (%%s, %%s);", obj.tables.node_index),
+		UPDATE_INDEX = string.format("UPDATE %s SET version=%%s WHERE id = %%s;", obj.tables.node_index),
+		GET_METADATA = string.format("SELECT id,version,timestamp,author,comment FROM %s WHERE id = %%s ORDER BY timestamp;", obj.tables.node),
+		GET_METADATA_LATEST = string.format("SELECT n.id,n.version,timestamp,author,comment FROM %s as n NATURAL JOIN %s WHERE n.id = %%s ORDER BY timestamp;", obj.tables.node, obj.tables.node_index),
 	}
-
-	-- Create the new object
-	local obj = {con=con, versium=versium}
-	setmetatable(obj, self)
-	self.__index = self
 
 	return obj 
 end
 
 -----------------------------------------------------------------------------
--- Returns a table representing the node with a given id.
+-- Returns the data stored in the node as a string and a table representing
+-- the node's metadata.  Returns nil if the node doesn't exist.  Throws an
+-- error if anything else goes wrong.
 --
--- @param id             the node id.
--- @param version        the desired version of the node (defaults to current).
--- @return               the node as table with its content in node.data.
+-- @param id             a node id.
+-- @param version        [optional] the desired version of the node (defaults
+--                       to current).
+-- @return               a byte-string representing the data stored in the
+--                       node or nil if the node could not be loaded or nil.
+-- @return               a table representing the metadata, including the
+--                       following fields (see get_node_history()) or nil.
+-- @see get_node_history
 -----------------------------------------------------------------------------
-function Storage:get_node(id, version)
+function MySQLVersium:get_node(id, version)
+   assert(id)
+
 	-- Get the most recent version of the node
 	local cmd
 	if version then
@@ -138,29 +161,15 @@ function Storage:get_node(id, version)
 
 	-- Run the query to get the node
 	local cur = assert(self.con:execute(cmd))
+   local row = cur:fetch({}, "a")
+   cur:close()
 
-	-- If we still don't have a version
-	if cur:numrows() ~= 1 then
-		versium.storage_error(version.errors.NODE_DOES_NOT_EXIST, tostring(id))
-	end
+   if not row then
+      return nil
+   end
 
-	local row = cur:fetch({}, "a")
 	assert(row.data)
-
-	return row
-end
-
------------------------------------------------------------------------------
--- Returns a table representing a node that doesn't actually exist yet.
---
--- @return               the stub of the node as a table.
------------------------------------------------------------------------------
-function Storage:get_stub(id)
-   return {
-      version = "000000",
-      data = "",
-      id = id,
-   }
+	return row.data, self:get_node_info(id, version)
 end
 
 -----------------------------------------------------------------------------
@@ -169,26 +178,64 @@ end
 -- @param id             an id of an node.
 -- @return               true or false.
 -----------------------------------------------------------------------------
-function Storage:node_exists(id)
+function MySQLVersium:node_exists(id)
 	assert(id)
 
 	local cmd = prepare(self.queries.NODE_EXISTS, id)
 	local cur = assert(self.con:execute(cmd))
-	local num = cur:numrows()
+   local row = cur:fetch({}, "*a")
 	cur:close()
 
-	return num == 1
+	return row ~= nil
 end
 
 -----------------------------------------------------------------------------
--- Returns a list of IDs of all nodes in the repository, in no particular order.
+-- Returns a table with the metadata for the latest version of the node. Same
+-- as get_node_history(id)[1] in case of this implementation.
 -- 
--- @return               a list of IDs.
+-- @param id             an id of an node.
+-- @return               the metadata for the latest version or nil.
 -----------------------------------------------------------------------------
-function Storage:get_node_ids()
+function MySQLVersium:get_node_info(id)
+	assert(id)
+
+	-- Fetch the latest version number
+	local cmd = prepare(self.queries.GET_METADATA_LATEST, id)
+	local cur = assert(self.con:execute(cmd))
+	local row = cur:fetch({}, "a")
+	cur:close()
+
+	return row
+end
+
+-----------------------------------------------------------------------------
+-- Returns a list of existing node ids, up to a certain limit.  (If no limit
+-- is specified, all ids are returned.)  The ids can be optionally filtered
+-- by prefix.  (In this case, the limit applies to the number of ids that
+-- are being _returned_.)  The ids can be returned in any order.
+--
+-- @param prefix         [optional] a prefix to filter the ids (defaults to
+--                       "").
+-- @param limit          [optional] the maximum number of ids to return.
+-- @return               a list of node ids.
+-- @return               true if there are more ids left.
+-----------------------------------------------------------------------------
+function MySQLVersium:get_node_ids(prefix, limit)
 	local nodes = {}
-	local cmd = prepare(self.queries.GET_NODES)
-	local cur = self.con:execute(cmd)
+   local cmd
+   if prefix and limit then
+      prefix = prefix .. "%"
+      cmd = prepare(self.queries.GET_NODES_PREFIX_LIMIT, prefix, limit)
+   elseif prefix then
+      prefix = prefix .. "%"
+      cmd = prepare(self.queries.GET_NODES_PREFIX, prefix)
+   elseif limit then
+      cmd = prepare(self.queries.GET_NODES_LIMIT, limit)
+   else
+      cmd = prepare(self.queries.GET_NODES)
+   end
+
+	local cur = assert(self.con:execute(cmd))
 	local row = cur:fetch({}, "a")
 
 	while row do
@@ -210,43 +257,25 @@ end
 -- @param extra          any extra metadata (optional).
 -- @return               the version id of the new node.
 -----------------------------------------------------------------------------
-function Storage:save_version(id, data, author, comment, extra, timestamp)
+function MySQLVersium:save_version(id, data, author, comment, extra, timestamp)
 	assert (id)
 	assert(data)
 	assert(author)
 
-	-- generate and save the new index
 	if not timestamp then
 		local t = os.date("*t")
 		timestamp = string.format("%02d-%02d-%02d %02d:%02d:%02d", t.year, t.month, t.day, t.hour, t.min, t.sec)
 	end
 
-	-- Escape all backslashes in the data, since it will be going through a gsub filter
-	data = data:gsub("\\", "\\\\")
-
-	-- Determine what the new version number will be
-	local cmd = prepare(self.queries.GET_VERSION, id)
-	local cur = self.con:execute(cmd)
-	local row = cur:fetch({}, "a")
-	cur:close()
-
-	local out = ""
-	for k,v in pairs(row) do
-		out = out .. "key: " .. tostring(k) .. " Value: " .. tostring(v)
-	end
-
-	local new
-	local version = row and tonumber(row.version)
-	if not version then
-		version = 1
-		new = true
-	else
-		version = version + 1
-	end
+   data = data:gsub("\\", "\\\\")
 
 	-- Store the new node in the 'nodes' table
 	local cmd = prepare(self.queries.INSERT_NODE, id, version, author, comment, timestamp, data)
-	local cur,err = assert(self.con:execute(cmd), out)
+	assert(self.con:execute(cmd), out)
+
+   local cur,err = assert(self.con:execute("SELECT LAST_INSERT_ID();"))
+   local version = assert(cur:fetch("*a"))
+   cur:close()
 	
 	-- Update the index table to the newest revision
 	if new then
@@ -274,7 +303,7 @@ end
 -- @return history  a list of tables representing the versions (the list will
 -- be empty if the node doesn't exist)
 -----------------------------------------------------------------------------
-function Storage:get_node_history(id, prefix)
+function MySQLVersium:get_node_history(id, prefix)
 	assert(id)
 
 	local history = {}
@@ -292,37 +321,6 @@ function Storage:get_node_history(id, prefix)
 	cur:close()
 
 	return history
-end
-
------------------------------------------------------------------------------
--- Returns a table with the metadata for the latest version of the node. Same
--- as get_node_history(id)[1] in case of this implementation.
--- 
--- @param id             an id of an node.
--- @return               the metadata for the latest version or nil.
------------------------------------------------------------------------------
-function Storage:get_node_info(id)
-	assert(id)
-
-	-- Fetch the latest version number
-	local cmd = prepare(self.queries.GET_METADATA_LATEST, id)
-	local cur = self.con:execute(cmd)
-	local row = cur:fetch({}, "a")
-	cur:close()
-
-	return row
-end
-
------------------------------------------------------------------------------
--- Creates a new storage object.
--- 
--- @param params         the parameters to pass to the implementation.
--- @param versium        a generic versium instance.
--- @return               the new versium storage object.
------------------------------------------------------------------------------
-
-function open(params, versium)
-   return Storage:new(params, versium)
 end
 
 -- vim:ts=3 ss=3 sw=3 expandtab

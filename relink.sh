82eb7fcc0f5380a74db82719f1e805eda51253a7
Yuri Takhteyev <yuri@freewisdom.org>
2008-04-20 23:19:56 -0700

    A script for re-linking the basic rocks at once.

---
+++ relink.sh
@@ -0,0 +1,9 @@
+
+rm /home/yuri/sputnik/rocks/versium/cvs-1
+rm /home/yuri/sputnik/rocks/saci/cvs-1
+rm /home/yuri/sputnik/rocks/sputnik/cvs-1
+
+bash link_rock.sh -r /home/yuri/sputnik/rocks/ -i /home/yuri/sputnik versium
+bash link_rock.sh -r /home/yuri/sputnik/rocks/ -i /home/yuri/sputnik saci
+bash link_rock.sh -r /home/yuri/sputnik/rocks/ -i /home/yuri/sputnik sputnik
+

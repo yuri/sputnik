257712154e7048009eadb60fcf10abfc3c5722d8
James Whitehead II <jnwhiteh@gmail.com>
2008-04-25 04:17:29 -0500

    Added anchors to the href/src patterns, and added absolute relative URLs (i.e. /blah)

--- xssfilter/lua/xssfilter.lua
+++ xssfilter/lua/xssfilter.lua
@@ -43,11 +43,11 @@ ALLOWED_TAGS = {
    -- For "a" tag allow "name" and "href", and limit href to three protocols.
    a = {
       name = ".",
-      href= {"http://", "https://", "ftp://"},
+      href= {"^http://", "^https://", "^ftp://", "^/"},
    },
    -- For "img" tag allow only "src" and limit it to http.
    img = {
-      src = "http://",
+      src = {"^http://", "^/"},
    },
    -- Style is allowed on "span" as long as the string "url" does not occur
    -- in the value

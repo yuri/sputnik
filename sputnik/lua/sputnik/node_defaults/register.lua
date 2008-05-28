13bbeb7c1a36b33f259eab08d11be49ae34894b0
Yuri Takhteyev <yuri@freewisdom.org>
2008-05-28 15:31:55 -0700

    Adding node_defaults/register.lua

---
+++ sputnik/lua/sputnik/node_defaults/register.lua
@@ -0,0 +1,10 @@
+module(..., package.seeall)
+
+NODE = {
+ actions = [[
+  show = "register.show_form"
+  submit = "register.submit"
+ ]],
+ --translations = [[_translations_register]],
+ title = "New Account",
+}

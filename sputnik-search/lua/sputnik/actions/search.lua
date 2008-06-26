29aaebc68b34c2c32a9cac0c1ee11b8a9b0460a2
Yuri Takhteyev <yuri@freewisdom.org>
2008-06-26 02:43:58 -0700

    Fixing search.

--- sputnik-search/lua/sputnik/actions/search.lua
+++ sputnik-search/lua/sputnik/actions/search.lua
@@ -2,19 +2,18 @@ module(..., package.seeall)
 
 actions = {}
 
-actions.show_results = function(page, request, sputnik)
-   local search_page = sputnik:get_node(page.name)
+actions.show_results = function(node, request, sputnik)
    if sputnik.config.SEARCH_API_KEY then
-       page.inner_html = cosmo.f(search_page.TEMPLATE){
+       node.inner_html = cosmo.f(node.content.TEMPLATE){
                             api_key = sputnik.config.SEARCH_API_KEY,
                             site    = sputnik.config.DOMAIN, --..sputnik.config.NICE_URL,
                             query   = request.params.q
                          }
    else 
-       page.inner_html = [[ 
+       node.inner_html = [[ 
           Please set <code>SEARCH_API_KEY</code> in sputnik.config to <b>your</b> 
           <a href="http://code.google.com/apis/ajaxsearch/signup.html">Google API key</a>.
        ]]
    end
-   return page.wrappers.default(page, request, sputnik)
+   return node.wrappers.default(node, request, sputnik)
 end

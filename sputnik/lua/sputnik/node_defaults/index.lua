d955b73ce2e4ad0ec049337ae552df5e71fdf8e4
Yuri Takhteyev <yuri@freewisdom.org>
2008-07-06 19:31:10 -0700

    Updating the index node.

--- sputnik/lua/sputnik/node_defaults/index.lua
+++ sputnik/lua/sputnik/node_defaults/index.lua
@@ -6,15 +6,34 @@ NODE = {
 
 NODE.content=[===[
 
-Congratulations, you just installed [Sputnik](http://www.freewisdom.org/projects/sputnik/)!  
+Congratulations, you just installed
+[Sputnik](http://www.freewisdom.org/projects/sputnik/)!  
 
 What's next? 
 
-First, check if you edit pages.  (Click "Edit" on this page.) You will need to login - just login with any user name and password and a new accout will be created for you.  <b>If you experience problems editing a page, check that your data directory is writable by your web server.</b>
+**First**, check if can you edit pages.  (Click the pencil icon on this page.)
+<b>If you experience problems editing a page, check that your data directory is writable by your web server.</b>
 
-After that, you can do some customizations.  Edit the [[_config]] page to change the title of your wiki, set the domain of your server, a URL for your logo, etc.  While there, you can also edit the ``NICE_URL`` parameter to whatever you want the your "short" URLs to start with.  E.g., if you set it to "/mysite/" your wikilinks will point to "/mysite/Page_Name".
+**Second**, click on "register" and create an account called "Admin". This
+account will be special - you will be able to use it to edit pages that are
+not editable by other users.  You can also create a "regualar" account for
+yourself, calling it something like "Bob" or "Andre".
 
-Also, edit the [[_navigation]] page to set your navigation bar.  If you are new to Lua, don't get scared.  Use the "preview" button to check if your navigation is correct. You can also change the color scheme of your wiki by editing [[_colors]].  (See the [Customization](http://www.freewisdom.org/projects/sputnik/Customization) page on the [Sputnik Wiki](http://www.freewisdom.org/projects/sputnik/) for more information.)
+After that, logged in as "Admin", you can do some customizations.
+
+Edit [[sputnik/config.edit]] page to change the title of your wiki, set the domain
+of your server, a URL for your logo, etc.  While there, you can also edit the
+``NICE_URL`` parameter to whatever you want the your "short" URLs to start
+with.  E.g., if you set it to "/mysite/" your wikilinks will point to
+"/mysite/Page_Name".
+
+Also, edit the [[sputnik/navigation.edit]] page to set your navigation bar.
+If you are new to Lua, don't get scared.  Use the "preview" button to check
+if your navigation is correct. You can also change the color scheme of your
+wiki by editing [[sputnik/css/colors.edit]]. (See the
+[Customization](http://www.freewisdom.org/projects/sputnik/Customization)
+page on the [Sputnik Wiki](http://www.freewisdom.org/projects/sputnik/) for
+more information.)
 
 ]===]
 

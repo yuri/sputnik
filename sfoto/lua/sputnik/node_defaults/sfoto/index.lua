e18922c61093a283abb5b3c360493a9b4b27d796
Yuri Takhteyev <yuri@freewisdom.org>
2008-07-22 20:39:40 -0700

    A prototype for sfoto index page.

---
+++ sfoto/lua/sputnik/node_defaults/sfoto/index.lua
@@ -0,0 +1,13 @@
+module(..., package.seeall)
+NODE = {
+   templates = "sfoto/templates",
+}
+NODE.fields = [[
+content.activate = "lua"
+]]
+NODE.actions = [[
+show="sfoto.show"
+]]
+
+NODE.content = [===[
+]===]

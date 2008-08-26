6381c32915587b37cd4e04f7a6c701a8e7e24856
Yuri Takhteyev <yuri@freewisdom.org>
2008-08-26 02:26:07 -0700

    Added a listing of all system nodes to "sputnik" node.

--- sputnik/lua/sputnik/node_defaults/sputnik/init.lua
+++ sputnik/lua/sputnik/node_defaults/sputnik/init.lua
@@ -6,12 +6,55 @@ NODE = {
 }
 NODE.content=[=====[
 
-This page shows links to Sputnik's configuration pages.
+Sputnik has a number of system nodes that control the behavior of your wiki as a whole.
+You can edit them if you are logged in as "Admin"
+
+## Pages You Definitely Want to Edit
 
 * [[sputnik/config]] - the main configuration page
-* [[_navigation]] - the navigation bar
-* [[sputnik/css/colors]] and [[sputnik/css/layout]] - stylesheets
-* [[sputnik/templates"]] and [[sputnik/translations]] - the text and templates
+* [[sputnik/navigation]] - the navigation bar
+
+## Maybe Those Too, At Some Point?
+
+* [[sputnik/passwords]] - the password file
+* [[sputnik/css/colors]] - a stylesheet for colors
+* [[sputnik/css/layout]] - a stylesheet for layout
+* [[sputnik/templates"]] - the templates
+* [[logo]] - the Sputnik logo (you can upload yours instead)
+
+## Less Likely
+
+* [[sputnik/translations]] - the text and templates
+* [[@Binary_File.lua]]
+* [[@CSS.lua]]
+* [[@JavaScript.lua]]
+* [[@Lua_Config.lua]]
+* [[@Root.lua]]
+* [[@Text_Config.lua]]
+
+## Those Probably Not
+
+* [[history]] - your history (also [[history.rss|history.rss]])
+* [[sitemap]] - the list of the pages (also [[sitemap.xml|sitemap.xml]])
+* [[register]] - the registration page
+* [[version]] - version information
+* [[sputnik/js/editpage.js|sputnik/js/editpage.js]] - javascript for the editor
+* [[sputnik/css/yui_reset]] - YUI reset library
+* [[sputnik/jquery]] - JQuery library
+* [[sputnik/jquer/textarearesizer]] - a script to resize the text area
+* [[sputnik/icons/admin]]
+* [[sputnik/icons/anon]]
+* [[sputnik/icons/attach]]
+* [[sputnik/icons/diff]]
+* [[sputnik/icons/edit]]
+* [[sputnik/icons/feed]]
+* [[sputnik/icons/feed_medium]]
+* [[sputnik/icons/history]]
+* [[sputnik/icons/lua]]
+* [[sputnik/icons/picture]]
+* [[sputnik/icons/sputnik]]
+* [[sputnik/icons/system]]
+* [[sputnik/icons/user]]
 
 ]=====]
 

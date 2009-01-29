7322481708f21b82945ed876cda99d80c1703cfa
Yuri Takhteyev <yuri@freewisdom.org>
2009-01-28 16:33:16 -0800

    Adding the star icon.

---
+++ sputnik/lua/sputnik/node_defaults/icons/star.lua
@@ -0,0 +1,35 @@
+-----------------------------------------------------------------------------
+-- A star icon.
+-- 
+-- Image (c) Mark James (http://famfamfam.com), 2008
+-- Creative Commons Attribution 2.5 License.
+-----------------------------------------------------------------------------
+
+
+module(..., package.seeall)
+
+
+
+NODE = {
+ prototype = "@Binary_File",
+ file_name = "star.png",
+ copyright = "Mark James (http://famfamfam.com)",
+ title     = "Star Icon",
+ actions   = [[png = "binaryfile.mimetype"]],
+ file_type = "image/png",
+}
+
+NODE.content = [[
+iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAABGdBTUEAAK/INwWK6QAAABl0RVh0U
+29mdHdhcmUAQWRvYmUgSW1hZ2VSZWFkeXHJZTwAAAIwSURBVDjLlZLNS5RRFMafe9/3vjPOjI1jaK
+KEVH40tGgRBWEibfoPQoKkVdtoEQQF4T/QqkVtWrSTFrVsF1FgJbWpIAh1k2PNh+PrfL4f95zTQk0
+HHKkDD/cc7vP8uHCuEhF0q/KnmXNgGR248PZFN4/GISXMC8L89DBPV0Dp4/SsazJjrtfb9/vdxfn/
+BgjzY5M8Aq8nBya+V3h93vtnQHFxat4kszntJAAAxus1YvnZQV5V/jyTEZarwnwFLGeFZdT0ZFOJd
+D84qoCDOpQ7grZfRNj020JSEOKvwvxGiF+q0tL0N5PuO+Mk0nC0B0BDsYCCImyzAIktBBloMwKJLS
+gKYcMAcdhC2KpVlIig+H5qxcv0n0xmj4Gbq+BwC2wtJLbgHUlMEFJwUpMIGpto16u+kJzSACAk+WC
+zvNbe+AVljkOYIcQQou3TbvdOJo+g4aNdqzaF+PT43HJVA8DQpcVIiPPtaqlEUQzlDELsTpgYwgTA
+QIjQqlUCtpQfn1spdmxh+PJSQyw9CrbKgM7tvcISQAxlBhC3GuCYXk3cWP25m3M7dk88qbWBRDVAp
+aATOSjPBdXXwYEP5QyCgvjE/kwHgInHtHYBnYA2owhrPiiuw0sOw3EZFEagIB7qChDiYaUcNIoFtP
+1KxCTPhWiDw7WbXk9vKpnOgsI4exjg6Mbq96YQPxm79uPOvqvbXx4O3KrF6w8osv2df17kr5YXJq7
+vnw/S0v3k7Ie7xtud/wAaRnP+Cw8iKQAAAABJRU5ErkJggg==
+]]

46719bcf7b2a4eef014c2fc04cd589f41af95de8
James Whitehead II <jnwhiteh@gmail.com>
2009-02-27 09:23:47 +0000

    Fixing path to jquery-textarearesizer in sputnik.editor.resizeable

--- sputnik/lua/sputnik/editor/resizeable.lua
+++ sputnik/lua/sputnik/editor/resizeable.lua
@@ -1,7 +1,7 @@
 module(..., package.seeall) --sputnik.editor.resizeable
 
 function initialize(node, request, sputnik)
-	node:add_javascript_link(sputnik:make_url("jquery/textarea", "js"))
+	node:add_javascript_link(sputnik:make_url("jquery/textarearesizer", "js"))
 	node:add_javascript_snippet[[
 $(document).ready(function() {
 	$('textarea.editor_resizeable').TextAreaResizer();

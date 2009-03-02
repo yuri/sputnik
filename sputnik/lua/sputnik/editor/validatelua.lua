79bc7ce887db83f59f067a2515bc12413b1e7b26
Yuri Takhteyev <yuri@freewisdom.org>
2009-03-02 00:04:12 -0800

    Adding a separate color for the case where validaction does not return either "valid" or "invalid" (gray by default), and making the colors configurable.

--- sputnik/lua/sputnik/editor/validatelua.lua
+++ sputnik/lua/sputnik/editor/validatelua.lua
@@ -1,30 +1,40 @@
 module(..., package.seeall) --sputnik.editor.resizeable
 
 local SNIPPET_TEMPLATE = [[
-  $(document).ready(function() {
-	// Store the timer id
-	var timerId = 0;
-	$("textarea.editor_validatelua").keyup(function (e) {
-		var field = this;
-		var code = $(this).val();
-		clearTimeout(timerId);
-		timerId = setTimeout(function() {
-			$.post("%s"  ,
-			{ p: "sputnik/js/editpage.validate_lua", code: code },
-			function(data) {
-				if (data == "valid")
-					$(field).css("background-color", "#D0F8D0");
-				else
-					$(field).css("background-color", "#F8E0E0");
-				});
-			}, 500);
-		});
-	});
+$(document).ready(function() {
+   // Store the timer id
+   var timerId = 0;
+   $("textarea.editor_validatelua").keyup(function (e) {
+      var field = this;
+      var code = $(this).val();
+      clearTimeout(timerId);
+      timerId = setTimeout(function() {
+         $.post("$url"  ,
+                { p: "sputnik/js/editpage.validate_lua", code: code },
+                function(data) {
+                   if (data == "valid") {
+                      $(field).css("background-color", "$valid");
+                   }
+                   else if (data == "invalid") {
+                      $(field).css("background-color", "$invalid");
+                   }
+                   else {
+                      $(field).css("background-color", "$unknown");
+                   }
+                }
+               );
+      }, 500);
+   });
+});
 ]]
 
-
 function initialize(node, request, sputnik)
-	node:add_javascript_snippet(
-       SNIPPET_TEMPLATE:format(sputnik.config.BASE_URL)
-    )
+   node:add_javascript_snippet(
+      cosmo.f(SNIPPET_TEMPLATE){
+         url = sputnik.config.BASE_URL,
+         valid = sputnik.config.LUA_VALIDATION_COLOR_VALID or "#D0F8D0",
+         invalid = sputnik.config.LUA_VALIDATION_COLOR_INVALID or "#F8E0E0",
+         unknown = sputnik.config.LUA_VALIDATION_COLOR_UNKNOWN or "#F8F8F8",
+      }
+   )
 end

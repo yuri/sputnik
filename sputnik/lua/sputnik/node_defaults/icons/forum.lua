0a7e3fce9d922235f8096766615b09fd5ebbc470
Yuri Takhteyev <yuri@freewisdom.org>
2009-03-10 13:08:02 -0700

    Adding an icon for forums.

---
+++ sputnik/lua/sputnik/node_defaults/icons/forum.lua
@@ -0,0 +1,45 @@
+-----------------------------------------------------------------------------
+-- An icon representing an forum.
+-- 
+-- Original Image Copyright (c)  2006-2007  Everaldo Coelho.
+-- (Modified by Jim Whitehead.)
+-- Distributed under GNU General Public License.
+-- http://www.everaldo.com/crystal/?action=license
+-----------------------------------------------------------------------------
+
+module(..., package.seeall)
+
+NODE = {
+ prototype = "@Image",
+ file_name = "discussion.png",
+ copyright = "Everaldo Coelho (http://www.everaldo.com, everaldo@everaldo.com",
+ title     = "Forum Icon",
+ file_type = "image/png",
+}
+
+NODE.content = [[
+iVBORw0KGgoAAAANSUhEUgAAABYAAAAWCAYAAADEtGw7AAAAAXNSR0IArs4c6QAAAAZiS0dEAP8A/
+wD/oL2nkwAAAAlwSFlzAAALEwAACxMBAJqcGAAAAAd0SU1FB9kDChM7LMHy4wEAAATcSURBVDjLnZ
+VriFVVFMf/a+199j7n3rkzOQ9HTY0yg4woKrL3c4QkoqCyD1EJFoGhfek1BkYQaJBEJEFZpGJk4SO
+1MswpMqkIJySrMXFmnPeM996575l77jl77z7YUJiSuD5uFj/+e//X/i/COdZzaz+46q7rrribIK6E
+E7PGywiyYZiLIpzI5is/vL7ivj0GkxNT/fR/QIFZdYcO7V2dCBJLAz/VorVGbqKGgUINUQyEkUGpP
+IlCsdzx9eGeV3evfeT7s4IPj47y1TNm2Ht2rZ/5fOudm6clGtrmtiRRqob2RLbqxicNxcaC4OAAZ5
+0UAoxCsVI6+FPPE5vW3LfzrIpF082pru82bG+dPmfRHyN5FMPIMjNVY0tMDADOWEuxAWpx7GqRs0n
+ti7FMobDj21/bxNnAB7754slZzS0rlHTIh5GtxGAhiBJaQiuGJ0BSMKRgeEwEYgqjyKbqUkESRvAU
+6IE1+9k5RwAglrzUWI7MQ1IpFKqRKUWWk75AKvCQ8AmBFgh84QLNCBRDK4GEFuRJIiksRKBvl1Pgz
+9rbLLUDi1/Ze9WNlzY8VbXyBuuAchQJKRkJLaAEQ/ytxTpGFAORiMHMACysFeQACCmb5D+XX1i38c
+CO1UHgL62Wiy3WTSC2gCMBXwGBEtAeIBggYlhrEUkgjCSIYlgwjHFwJMBkQgkA8z7eMfOZ1ps2O5V
+sa2pUGDGhrZTKVDGWiIFACmjF8D1AeQyA4CyjFlswWwASsbFw2tpyFaJarXZK1D+SWjPv5k2t0xvb
+ukYm8cvAhGVOkDZFSpcmUF+nIMUpoK8ZvifBAohiQxwxrIsRxxZKWsvsi/7RDNIn01t5/c43H6tPp
+RaRM/A8WMvEJIhskMLYeA7VWEJ5CoIYguiUcsEIlIQnGZ5kpxVD64AzuUkMD/VvfKf94Y9kxbgHpR
+IoVGNTipxI+gwtGcz1KIUxculuaDRBTbsAYAVrDQQLCFIQwoAYFEYWoyMZ9B0/sWXVl1tXGsDJmuE
+F1gGV2LKUhIRmeIIgBAF+M4ZL5Mp9x2l+dQzBhXMxQfXQklGdyKFYmkS2ENp0tnj06LHeN55dtuRD
+ANh/oJNkbKBOuU/wFcFX/C/3CVY1I7qgGZ2ZQVw+PIZLLmpCITuEI30ldPUVyoM93WsHf9z39vbPP
+ylOzVfbbdc6WYtrvRXjGonhfMHka3K+B3iSiIicNQ612CFEDaXxkxjxFDpPajscX8qHM4PfffryC2
+8BP5dP/7kch9W9mVINhhiehPMkkVZMCS2QDJiCgKE8wAuLyLsUOgvTbC05m3P5CrLp7NYzQQFALLj
+mxu6gofVeP1nXnAyYtCSnPULgMyU0gYhgLIiSzc7WzyYjkjSSDtHbM7Bx14sLXztb1vD7qx7tp2L3
+snJ6oL9SqsBYR46InHPOAWDBJKWEUJrCiDE2UsHA8f4tW/ZuWwnA3btuF585xwF0bNvUP6fR36G03
+xIovjiRCJRlaY1lrpRC5HOTyGQmzfDw+NE/u7pffG/59atx7Pvauq/6af3jC92ZwP/J45XLn7jslm
+Xt7zbOmX+HreTHRvsG9gyN5gZHx/K/duw+2PFbx7riuawyefpBfsY1c4Yz5WvJGxka7z2y9On7F+/
+HedTp78MNiYYFpWx6w+8/7rv1fKEA8BdNSzv0yitupwAAAABJRU5ErkJggg==
+]]

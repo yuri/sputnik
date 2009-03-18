5ec8dd438dd9fc58c765101364efceea20db7943
James Whitehead II <jnwhiteh@gmail.com>
2009-03-18 09:26:49 +0000

    Add markitup to comment editor

--- sputnik/lua/sputnik/node_defaults/@Comment.lua
+++ sputnik/lua/sputnik/node_defaults/@Comment.lua
@@ -19,7 +19,7 @@ reset()
 comment_parent = {3.00, "hidden", div_class="hidden", no_label=true}
 --comment_author = {3.001, "text_field"}
 --title          = {3.002, "text_field"}
-content        = {3.01, "textarea", rows=15, no_label=true, editor_modules = {"resizeable"}}
+content        = {3.01, "textarea", rows=15, no_label=true, editor_modules = {"resizeable", "markitup"}}
 ]]
 NODE.admin_edit_ui = [[
 comment_section  = {1.401, "div_start", id="comment_section", closed="true"}

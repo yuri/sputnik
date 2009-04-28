c8ad90c112569df60914b2b0227a52a2f884e1a5
James Whitehead II <jnwhiteh@gmail.com>
2009-04-28 14:52:11 +0000

    Enable the XML action for sitemaps

--- sputnik/lua/sputnik/node_defaults/sitemap.lua
+++ sputnik/lua/sputnik/node_defaults/sitemap.lua
@@ -3,7 +3,8 @@ module(..., package.seeall)
 NODE = {
    title="Sitemap",
    category="_special_pages",
-   actions=[[ show="wiki.list_nodes"; xml="wiki.show_sitemap_xml" ]]
+   actions=[[ show="wiki.list_nodes"; xml="wiki.show_sitemap_xml" ]],
+   permissions=[[ allow(all_users, "xml") ]],
 }
 NODE.content=[=====[
 The content of this page will be ignored.

438e170ca4c537b3d62b1787bb6fa1b02bb2abdd
Yuri Takhteyev <yuri@freewisdom.org>
2009-07-31 18:42:01 -0400

    Adding support for template handlers.

--- sputnik-tickets/lua/sputnik/node_defaults/@Ticket.lua
+++ sputnik-tickets/lua/sputnik/node_defaults/@Ticket.lua
@@ -1,9 +1,10 @@
 module(..., package.seeall)
 NODE = {
-   actions= [[show = "tickets.show"]],
+   --actions= [[show = "tickets.show"]],
    icon = "icons/bug.png",
    translations = "tickets/translations",
    templates    = "tickets/templates",
+   prototype = "@Discussion",
 }
 
 NODE.permissions = [[
@@ -20,6 +21,25 @@ prod_version = {.16}
 component   = {.17}
 assigned_to = {.19}
 resolution_details = {.20}
+template_helpers = {activate="lua", proto="concat"}
+]]
+
+NODE.template_helpers = [[
+function z(params)
+   return "[."..params[1].."]"
+end
+
+function priority_to_stars(params)
+   local config = {
+     unassigned = "",
+     highest    = "★★★★",
+     high       = "★★★ ",
+     medium     = "★★",
+     low        = "★",
+     lowest     = ".",
+   }
+   return config[params[1] ]
+end
 ]]
 
 NODE.edit_ui= [[
@@ -43,4 +63,59 @@ breadcrumb  = null
 
 ]]
 
+NODE.html_content = [======[
+<table width="100%">
+ <tr style="background:$ticket_status_color">
+  <td width="15%" style="text-align: right;">
+	   <span style="font-size: 80%">$z{"bar"} ticket id</span><br/>
+   <span style="font-size: 200%;">$ticket_id</span>
+  </td>
+  <td width="15%" style="text-align: right;">
+   <span style="font-size: 80%">status</span><br/>
+   <span style="font-size: 200%">$status</span>
+  </td>
+  <td width="15%" style="text-align: right;">
+   <span style="font-size: 80%">priority $priority</span><br/>
+   <span style="font-size: 200%">$priority_to_stars{$priority}</span>
+  </td>
+  <td width="15%" style="text-align: right;">
+   <span style="font-size: 80%">assigned to</span><br/>
+   <span style="font-size: 200%">$assigned_to</span>
+  </td>
+  <td width="40%" style="text-align: right;">
+   Reported by: $reported_by<br/>
+   Component: $component
+  </td>
+ </tr>
+</table>
+<br/>
 
+$markup{$content}
+
+<br/><br/><br/>
+<a href="$make_url{$new_id, "edit", comment_parent=$id}">Post a comment</a>
+<br/><br/><br/>
+
+<ol class="discussion">
+ $do_nodes[====[
+ <a name="$short_id"></a><li class="reply">
+  <div class="post-header">
+  <span class="post-info">
+   Posted by $comment_author on $format_time{$creation_time, "%a, %d %b %Y %H:%M:%S"}
+  </span>
+  <ul class="post-toolbar">
+   $if_user_can_edit[[<li><a href="$edit_link">_(EDIT)</a></li>]]
+   $if_user_can_configure[[<li><a href="$configure_link">_(CONFIGURE)</a></li>]]
+   <li><a href="$make_url{$new_id, "edit", comment_parent=$id}">_(REPLY)</a></li> 
+   <li><a href="$make_url{$new_id, "edit", comment_parent=$id, quote="true"}">_(QUOTE)</a></li> 
+  </ul>
+  </div>
+  <div class="content">
+   $markup{$content}
+  </div>
+ </li>
+ ]====]
+</ol>
+
+<br/>
+]======]

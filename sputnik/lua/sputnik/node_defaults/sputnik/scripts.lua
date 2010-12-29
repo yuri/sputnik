a809105509a4131a0b871505b870c047d769ee9b
Yuri Takhteyev <yuri@freewisdom.org>
2010-12-29 10:56:12 -0500

    Escaping $ in the scripts template.

--- sputnik/lua/sputnik/node_defaults/sputnik/scripts.lua
+++ sputnik/lua/sputnik/node_defaults/sputnik/scripts.lua
@@ -18,55 +18,55 @@ NODE.content = [======[
 $jquery
 
 function sputnik_init_page() {
-  $(".field input, .field textarea").not(".submit").focus(
-    function(){$(this).addClass("active_input");}
+  $$(".field input, .field textarea").not(".submit").focus(
+    function(){$$(this).addClass("active_input");}
   ).blur(
-    function() {$(this).removeClass("active_input");}
+    function() {$$(this).removeClass("active_input");}
   );
-  $(".autofocus input").focus();
+  $$(".autofocus input").focus();
 }
 
 function sputnik_make_modal_popup(id, url) {
    var login_form = document.createElement('div');
    var selector = "#"+id;
-   $(selector).fadeIn();
-   $(selector).append('<div class="transparency">&nbsp;</div>');
-   $(selector).append('<div class="popup_frame"><div class="close_popup">⨯</div><div class="actual_form"/></div>');
-   $(selector+" div.actual_form").load(url);
-   $(selector+" .close_popup").click(
-    function(){ $(selector).hide(); return false; }
+   $$(selector).fadeIn();
+   $$(selector).append('<div class="transparency">&nbsp;</div>');
+   $$(selector).append('<div class="popup_frame"><div class="close_popup">⨯</div><div class="actual_form"/></div>');
+   $$(selector+" div.actual_form").load(url);
+   $$(selector+" .close_popup").click(
+    function(){ $$(selector).hide(); return false; }
    );
    sputnik_init_page();
 }
 
-$(document).ready(function(){
+$$(document).ready(function(){
 
  sputnik_init_page();
 
- $("#sidebar ul#menu > li > a ").click(
+ $$("#sidebar ul#menu > li > a ").click(
   function(){
-   $(this).siblings("ul").slideToggle();
+   $$(this).siblings("ul").slideToggle();
    return false;
   }
  );
 
- $(".ctrigger").click(
+ $$(".ctrigger").click(
   function () {
    var selector = "#" + this.id.substring(8);
-   $(selector).slideToggle();
-   $(this).toggleClass("closed");
+   $$(selector).slideToggle();
+   $$(this).toggleClass("closed");
   }
  );
 
  // Actually hide all the closed elements
- $(".ctrigger.closed").each(
+ $$(".ctrigger.closed").each(
   function() {
    var selector = "#" + this.id.substring(8);
-   $(selector).hide();
+   $$(selector).hide();
   }
  );
 
- /*$("a.login_link").click(
+ /*$$("a.login_link").click(
   function(){
    sputnik_make_modal_popup("login_form", "/sputnik2.ws?p=sputnik/login&skip_wrapper=1");
    return false;

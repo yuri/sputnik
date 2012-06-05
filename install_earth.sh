bf4a51f0875a83b86b91f57d74d6f44c6b905e00
Yuri Takhteyev <yuri@freewisdom.org>
2012-06-04 22:36:41 -0400

    Removing some old files and directories.

--- install_earth.sh
+++
@@ -1,6 +0,0 @@
-# Install Kepler + Sputnik
-wget http://luaforge.net/frs/download.php/3468/kepler-install-1.1-1 
-bash kepler-install-1.1-1 --prefix=${PWD} --without-readline
-./bin/luarocks --only-from=http://sputnik.freewisdom.org/rocks/earth install sputnik 8.08.08
-./bin/lua -lluarocks.require -e 'require("sputnik").setup()'
-mkdir wiki-data && chmod -R a+rw wiki-data # Make wiki-data directory
